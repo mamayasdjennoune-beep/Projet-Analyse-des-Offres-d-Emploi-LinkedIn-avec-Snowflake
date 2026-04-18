35c8552785697feadcbdea05471024e847cdb1c9
mamayasdjennoune-beep <m.amayas.djennoune@gmail.com>
2026-04-18 21:39:36 +0200

    Update linkedin_streamlit_application.py

--- linkedin_streamlit_application.py
+++ linkedin_streamlit_application.py
@@ -1,4 +1,4 @@
-# Travail fait par : ZINET Sara, DJENNOUNE Mouloud-Amayas
+# Travail effectué par : ZINET Sara, DJENNOUNE Mouloud-Amayas
 
 #----------------------------------------------------------------------------------------------------------------
 
